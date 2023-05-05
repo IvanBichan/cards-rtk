a178ac5f351d474c8c3a2c8ef23d28f07d77b1ab
IvanBichan <ivanbichanfe@gmail.com>
2023-05-05 11:57:06 +0300

    feat: create project

---
+++ src/app/app.slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const slice = createSlice({
+  name: "app",
+  initialState: {
+    error: null as string | null,
+    isLoading: true,
+    isAppInitialized: false,
+  },
+  reducers: {},
+});
